7c13d22d8d841741d0659d20b2e141a1ec7bcbf5
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-07 14:00:30 +0200

    Init red-black-tree project

---
+++ rbTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by luke-masko on 8/7/26.
+//
+
+#include "rbTree.h"
---
+++ rbTree.h
@@ -0,0 +1,13 @@
+//
+// Created by luke-masko on 8/7/26.
+//
+
+#ifndef RED_BLACK_TREE_RBTREE_H
+#define RED_BLACK_TREE_RBTREE_H
+
+
+class rbTree {
+};
+
+
+#endif //RED_BLACK_TREE_RBTREE_H
