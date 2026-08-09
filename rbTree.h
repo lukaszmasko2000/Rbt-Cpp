af319d45d9dd8479e350d74d2519cad2015ba10b
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-09 18:23:35 +0200

    Dodanie klasy RedBlackTree (LM)

--- rbTree.h
+++ rbTree.h
@@ -6,7 +6,7 @@
 
 #include <iostream>
 
-template <typename T>
+template <class T>
 class RedBlackTreeNode {
 public:
     T data;
@@ -15,6 +15,28 @@ public:
 
     explicit RedBlackTreeNode(T data)
     : data(data), left(nullptr), right(nullptr) {};
+};
+
+template <class T>
+class RedBlackTree {
+public:
+    RedBlackTree() {
+        NIL = new RedBlackTreeNode<T>;
+        NIL->color = "black";
+        NIL->left = NIL->right = NIL;
+        root = NIL;
+    }
+
+    void insert(T data);
+    void inorderTraversal(RedBlackTreeNode<T> *node);
+    [[nodiscard]] RedBlackTreeNode<T> *getRoot() const { return root; };
+
+private:
+    RedBlackTreeNode<T> *root = nullptr;
+    RedBlackTreeNode<T> *NIL = nullptr;
+    void leftRotate(RedBlackTreeNode<T> *&node);
+    void rightRotate(RedBlackTreeNode<T> *&node);
+    void fixInsert(RedBlackTreeNode<T> *&node);
 };
 
 
