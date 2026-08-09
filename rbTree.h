121c6337e48c501190f96381cc99893ade5c2725
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-09 18:40:52 +0200

    Dodanie definicji funcji right and left Rotate (LM)

--- rbTree.h
+++ rbTree.h
@@ -27,17 +27,48 @@ public:
         root = NIL;
     }
 
-    void insert(T data);
-    void inorderTraversal(RedBlackTreeNode<T> *node);
+    void insert(T);
+    void inorderTraversal(RedBlackTreeNode<T> *);
     [[nodiscard]] RedBlackTreeNode<T> *getRoot() const { return root; };
 
 private:
     RedBlackTreeNode<T> *root = nullptr;
     RedBlackTreeNode<T> *NIL = nullptr;
-    void leftRotate(RedBlackTreeNode<T> *&node);
-    void rightRotate(RedBlackTreeNode<T> *&node);
-    void fixInsert(RedBlackTreeNode<T> *&node);
+    void leftRotate(RedBlackTreeNode<T> *&);
+    void rightRotate(RedBlackTreeNode<T> *&);
+    void fixInsert(RedBlackTreeNode<T> *&);
 };
+
+
+template <class T>
+void RedBlackTree<T>::leftRotate(RedBlackTreeNode<T> *&x) {
+    RedBlackTreeNode<T> *y = x->right;
+    x->right = y->left;
+
+    if (x->parent == nullptr)  root = x;
+    else if (x == x->parent->left) x->parent->left = y;
+    else x->parent->right = y;
+
+    y->left = x;
+    x->parent = y;
+}
+
+template <class T>
+void RedBlackTree<T>::rightRotate(RedBlackTreeNode<T> *&x) {
+    RedBlackTreeNode<T> *y = x->left;
+    x->left = y->right;
+
+    if (y->right != NIL)  y->rigth->parent = x;
+    y->parent = x->parent;
+
+    if (x->parent == nullptr)  root = y;
+    else if (x == x->parent->right) x->parent->right = y;
+    else x->parent->left = y;
+
+    y->right = x;
+    x->parent = y;
+}
+
 
 
 #endif //RED_BLACK_TREE_RBTREE_H
